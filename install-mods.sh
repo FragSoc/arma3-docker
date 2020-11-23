fac2693c3a812a2bd4ea322f9bb9fe5f931b1f94
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2020-11-23 17:21:04 +0100

    Temporarily disable decapitalisation

--- install-mods.sh
+++ install-mods.sh
@@ -26,4 +26,4 @@ steamcmd +login $STEAM_USER +force_install_dir /tmp $install_command +quit
 find "$TMP_DIR_FULL" -maxdepth 1 -mindepth 1 -type d -exec sh -c 'mv {} $MODS_LOC/\@$(basename {})' \;
 
 # Lowercase all the mods files
-find "$MODS_LOC" -depth -regex '.*[A-Z].*' -exec sh -c 'mv {} $(echo {} | tr A-Z a-z)' \;
+# find "$MODS_LOC" -depth -regex '.*[A-Z].*' -exec sh -c 'mv {} $(echo {} | tr A-Z a-z)' \;
