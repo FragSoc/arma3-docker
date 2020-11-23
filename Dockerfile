e35bd82fcd66284ffd3ea8bec1d4cc0ee66f57c1
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2020-11-23 19:14:22 +0100

    Hopefully fix mod installation by simplifying script

--- Dockerfile
+++ Dockerfile
@@ -15,7 +15,7 @@ USER root
 
 # Game appears to require ifconfig
 RUN apt-get update
-RUN apt-get install --no-install-recommends -y net-tools
+RUN apt-get install --no-install-recommends -y net-tools rename
 
 # Setup directory structure and permissions
 RUN useradd -m -s /bin/false -u $UID arma
--- install-mods.sh
+++ install-mods.sh
@@ -4,9 +4,6 @@ set -eo pipefail
 # Necessary variables
 # This needs to be the appid of the client, not the server
 APP_ID=107410
-# Steam creates it's own folder hierarchy when we download workshop items so we need to extract them
-TMP_DIR=/tmp
-TMP_DIR_FULL="$TMP_DIR/steamapps/workshop/content/$APP_ID/"
 # Mods can only be downloaded by someone who owns the game so we need a username to login with
 # Note that this means the script must be run interactively
 STEAM_USER=$1
@@ -16,14 +13,15 @@ MODS_IDS="$@"
 
 # Generate the single command to download all the mods
 install_command=""
-for mod_id in "$@"; do
+for mod_id in "$MODS_IDS" do
   install_command="$install_command +workshop_download_item $APP_ID $mod_id"
 done
 
 # Download the mods with steamcmd
 steamcmd +login $STEAM_USER +force_install_dir /tmp $install_command +quit
-# Move from temporary folder to mods folder and prepend with an @
-find "$TMP_DIR_FULL" -maxdepth 1 -mindepth 1 -type d -exec sh -c 'mv {} $MODS_LOC/\@$(basename {})' \;
 
-# Lowercase all the mods files
-# find "$MODS_LOC" -depth -regex '.*[A-Z].*' -exec sh -c 'mv {} $(echo {} | tr A-Z a-z)' \;
+# Move into mods folder and fix capitalisation
+for mod in "$MODS_IDS" do
+  mv "/tmp/steamapps/workshop/content/$APP_ID/$mod" "$MODS_LOC/\@$mod"
+  ( cd "$MODS_LOC/\@$mod"; find . -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \; )
+done
